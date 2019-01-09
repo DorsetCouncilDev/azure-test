b8367e519ea6789401b21604aa400ccd776034d7
Adam Burton <adamburton27@hotmail.com>
2019-01-09 12:03:41 +0000

    add rule

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const express = require("express");
 
-
 var app = express();
 var router = require("./routes/routes.js");
 app.use("/advertise",router);
-
-
-
-
-
-
-
 
 var port = process.env.PORT || 1337;
 app.listen(port);
